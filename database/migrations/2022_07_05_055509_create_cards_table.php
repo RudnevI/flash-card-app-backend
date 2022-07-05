0a3f21910b2babf16b82e5b516bb039b7ecbdce5
irudnev128@gmail.com <irudnev128@gmail.com>
2022-07-05 12:13:21 +0600

    init

---
+++ database/migrations/2022_07_05_055509_create_cards_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    private array $textFields = ['name', 'question', 'correct_answer'];
+
+
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            foreach ($this->textFields as $field) {
+                $table->text($field);
+            }
+            $table->date("repeat_date");
+            $table->foreignId("collection_id")->references("id")->on("collections")->onUpdate("cascade")->onDelete("cascade");
+            $table->foreignId("status_id")->references("id")->on("statuses")->onUpdate("cascade")->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};
